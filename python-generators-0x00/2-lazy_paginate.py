2a54975bebc61e90e9d60ce4e95b9fbd92e735ba
Mxolisi Khoza <mxobitcoin@gmail.com>
2025-07-04 00:02:57 +0200

    Update 2-lazy_paginate.py

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -1,1 +1,20 @@
 
+seed = __import__('seed')
+
+
+def paginate_users(page_size, offset):
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+
+
+try:
+    for page in lazy_paginator(100):
+        for user in page:
+            print(user)
+
+except BrokenPipeError:
+    sys.stderr.close() 
