b2e8fed0a4b2660ecefeb97cddd3976d659c87e8
Mxolisi Khoza <mxobitcoin@gmail.com>
2025-07-24 02:52:02 +0200

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,6 +1,9 @@
+pythonCopy codefrom django.urls import path
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
+from .views import BookListCreateAPIView
+
 
 # Create a router and register our viewsets with it
 router = DefaultRouter()
@@ -9,5 +12,6 @@ router.register(r'messages', MessageViewSet, basename='message')
 
 # The API URLs are now determined automatically by the router
 urlpatterns = [
+    path("api/books", views.BookListCreateAPIView.as_view(), name="book_list_create"),
     path('', include(router.urls)),
 ]
