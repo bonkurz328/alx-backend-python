bcd5b5d70e79b4cc81e357b5f7431ea42dbcb2f7
Mxolisi Khoza <mxobitcoin@gmail.com>
2025-07-24 02:36:14 +0200

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,10 +1,10 @@
-"""
-URL configuration for messaging_app project.
-"""
-from django.contrib import admin
-from django.urls import path, include
+""" 
+URL configuration for messaging_app project. 
+""" 
+from django.contrib import admin 
+from django.urls import path, include 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),
+urlpatterns = [ 
+    path('admin/', admin.site.urls), 
+    path('api/', include('chats.urls')), # Include chats app URLs under /api/ 
 ]  
