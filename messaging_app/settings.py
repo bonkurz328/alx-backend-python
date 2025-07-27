d05bf9c9d424abedc79ec678e965bfd2df718038
Mxolisi Khoza <mxobitcoin@gmail.com>
2025-07-27 18:02:31 +0200

    Update settings.py

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -12,6 +12,17 @@ INSTALLED_APPS = [
     'chats',
 ]
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.core.middleware.RequestLoggingMiddleware', 
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
