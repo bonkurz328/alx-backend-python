3442ed7ee5017c6fe776c83e1eadbacfe043fe22
Mxolisi Khoza <mxobitcoin@gmail.com>
2025-07-27 02:33:07 +0200

    Create permissions.py

---
+++ messaging_app/chats/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any authenticated user
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions are only allowed to the owner of the message/conversation
+        return obj.user == request.user
