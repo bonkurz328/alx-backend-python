45109b32093ac0d28d683f77ef1ef639a41ba80d
Mxolisi Khoza <mxobitcoin@gmail.com>
2025-08-02 16:31:40 +0200

    Update signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,7 +1,8 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
 from .models import Message, Notification, MessageHistory
 from django.contrib.auth import get_user
+from django.contrib.auth.models import User
 
 @receiver(post_save, sender=Message)
 def create_message_notification(sender, instance, created, **kwargs):
@@ -10,7 +11,7 @@ def create_message_notification(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance,
         )
-
+        
 @receiver(pre_save, sender=Message)
 def log_message_edit(sender, instance, **kwargs):
     if instance.pk:  # Check if this is an update (not a create)
@@ -31,5 +32,14 @@ def log_message_edit(sender, instance, **kwargs):
         except Message.DoesNotExist:
             pass  # Handle case where message doesn't exist yet
 
+@receiver(post_delete, sender=User)
+def cleanup_user_data(sender, instance, **kwargs):
+    # Delete related messages (both sent and received)
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+    # Delete related notifications
+    Notification.objects.filter(user=instance).delete()
+    # Delete related message histories
+    MessageHistory.objects.filter(editor=instance).delete()
 
 
